a957effdd1d98e0e8dc7e3e66d7e5f63a91cd76d
buyoh <japanjetone@live.jp>
2022-01-11 22:44:08 +0900

    Move app to src

--- jest.config.js
+++ jest.config.js
@@ -12,7 +12,7 @@ if (testTarget) {
 }
 
 module.exports = {
-  roots: ['<rootDir>/app'],
+  roots: ['<rootDir>/src'],
   transform: {
     '^.+\\.(ts|tsx)$': 'ts-jest',
   },
@@ -24,7 +24,7 @@ module.exports = {
   },
   moduleNameMapper: {
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
-      '<rootDir>/app/__mocks__/fileMock.js',
-    '\\.(css|less)$': '<rootDir>/app/__mocks__/styleMock.js',
+      '<rootDir>/src/__mocks__/fileMock.js',
+    '\\.(css|less)$': '<rootDir>/src/__mocks__/styleMock.js',
   },
 };
